8fa0ceeeadd9bd835ef7b56b35177ad773457e50
Renan <renan_battlenet@hotmail.com>
2018-09-19 17:45:37 -0300

    Push Automatico

--- lista_sachetti/mercearia.c
+++ lista_sachetti/mercearia.c
@@ -16,12 +16,14 @@ typedef struct lista_produtos {
 void insertProduto(LProdutos* p, Produto pr){
 	pr.id = p->length;
 	p->length++;
-	p->p = (Produto*) realloc(p->p, sizeof(Produto*) * p->length);
+	p->p = (Produto*) realloc(p->p , sizeof(Produto) * p->length);
 	p->p[p->length - 1] = pr;
+	printf("%s inserido\n", p->p[p->length-1].nome);
 }
 
 void selectProduto(LProdutos* p){
 	int i = 0;
+	printf("Tamanho: %d\n", p->length);
 	for( i = 0 ; i < p->length ; i++ ){
 		printf("[%d] %s\n", p->p[i].id, p->p[i].nome);
 		printf("Preco: %f | ", p->p[i].preco);
@@ -36,12 +38,28 @@ LProdutos* createList(){
 }
 
 void removeProduto(LProdutos* p){
+	int index = -1;
+	selectProduto(p);
 
+	printf("Insira o indice do produto que deseja remover:\n");
+	scanf("%d", &index);
+
+	shiftProdutos(p, index);
+
+}
+
+void shiftProdutos(LProdutos* p, int index){
+	int i = 0;
+	if ( index == 0 ) return;
+	for( i = index ; i <= p->length ; i++ ){
+		p->p[i-1] = p->p[i];
+	}
 }
 
 void alterProduto(LProdutos* p){
 
 }
+
 
 Produto readProduto(){
 	Produto p;
@@ -59,36 +77,36 @@ Produto readProduto(){
 int main(void){
 	int operacao = 0;
 
-	LProdutos* lista_produtos;
-	lista_produtos = createList();
-	Produto* p;
-
-	printf("\n");
-	printf("1- Cadastrar novo produto\n");
-	printf("2- Aumentar estoque\n");
-	printf("3- Diminuir estoque\n");
-	printf("4- Remover produto\n");
-	printf("5- Listar produtos\n");
-	printf("6- Sair\n");
-	scanf("%d", &operacao);
-	switch(operacao){
-		case 1:
-			p = readProduto();
-			insertProduto(lista_produtos, *p);
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			selectProduto(lista_produtos);
-			break;
-		case 6: return 0;
-		default:
-			printf("Opção invalida\n");
-			break;
+	LProdutos* lista_produtos = createList();
+	Produto p;
+	while ( 1 ) {
+		printf("\n");
+		printf("1- Cadastrar novo produto\n");
+		printf("2- Aumentar estoque\n");
+		printf("3- Diminuir estoque\n");
+		printf("4- Remover produto\n");
+		printf("5- Listar produtos\n");
+		printf("6- Sair\n");
+		scanf("%d", &operacao);
+		switch(operacao){
+			case 1:
+				p = readProduto();
+				insertProduto(lista_produtos, p);
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				removeProduto(lista_produtos);
+				break;
+			case 5:
+				selectProduto(lista_produtos);
+				break;
+			case 6: return 0;
+			default:
+				printf("Opção invalida\n");
+				break;
+		}
 	}
-	main();
 }
